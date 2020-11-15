1f3da8b4eeaee22740e7b3dfbe2976e1b1e17e23
Ankush Mishra <ankushmishra0939@gmail.com>
2020-11-15 12:07:16 +0530

    mongo module

--- Node-mongo/index.js
+++ Node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbpoper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,26 +12,25 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    dbpoper.insertDocument(db, { name: 'vadonut', description: 'Test'}, 'dishes',(result) => {
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+        console.log('Insert Document: \n', result.ops);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+        dbpoper.findDocument(db, 'dishes', (docs) => {
+            console.log('Found documents:\n',docs);
 
-                client.close();
+            dbpoper.updateDocument(db, {name: 'vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
+                console.log('Updated Document:\n',result.result);
+
+                dbpoper.findDocument(db, 'dishes', (docs) => {
+                    console.log('Found Document:\n',docs);
+
+                    db.dropCollection('dishes',(result) => {
+                        console.log('Dropped Collection: ',result);
+                    });
+                });
             });
         });
     });
-
 });
