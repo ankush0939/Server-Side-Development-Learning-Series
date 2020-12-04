375e795a3140955e33820820261210cef0468e9b
Ankush <ankushmishra0939@gmail.com>
2020-12-04 12:50:09 +0530

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,16 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishrouter');
 var promotionRouter = require('./routes/promotionrouter');
 var leaderRouter = require('./routes/leaderrouter');
-
-
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
 
@@ -44,26 +44,20 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
 
-  if(!req.session.user) {
+  if(!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
       return next(err);
   }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+  else {next();
   }
 }
 
