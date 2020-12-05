3a7303960fda6abaad61065b07b5618b2dc8c1b6
Ankush <ankushmishra0939@gmail.com>
2020-12-05 10:12:21 +0530

    passport jwt

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,7 +1,41 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+var JwStrategy = require('passport-jwt').Strategy;
+var ExtraJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken');
+
+var config = require('./config');
+const { ExtractJwt } = require('passport-jwt');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretkey, {
+        expiresIn: 3600
+    });
+};
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretkey;
+
+exports.jwtPassport = passport.use(new JwStrategy(opts, 
+    (jwt_payload, done) => {
+        console.log("JWT payload: ",jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if(err) {
+                return done(err, false);
+            }
+            else if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+exports.verifyuser = passport.authenticate('jwt', {session: false});
--- conFusionServer/routes/leaderrouter.js
+++ conFusionServer/routes/leaderrouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders');
 
@@ -16,7 +17,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req,res,next) => {
+.post(authenticate.verifyuser, (req,res,next) => {
     Leaders.create(req.body)
     .then((lead) => {
         console.log('Leader created', lead);
@@ -26,11 +27,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req,res,next) => {
+.put(authenticate.verifyuser, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
 })
-.delete((req,res,next) => {
+.delete(authenticate.verifyuser, (req,res,next) => {
     Leaders.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -50,11 +51,11 @@ leaderRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyuser, (req, res, next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyuser, (req, res, next) => {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, {new: true})
@@ -65,7 +66,7 @@ leaderRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyuser, (req, res, next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((lead) => {
         res.statusCode = 200;
